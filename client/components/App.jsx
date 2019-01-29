c5c8303e76fc665de967d28611bc23cadc533416
Jessica-Morgan <jess-morgan1@outlook.com>
2019-01-29 15:50:06 +1300

    multicolors showing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Pixel from './Pixel'
 
 const App = () => {
   return (
-    <div>Yup.</div>
+    <div>
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+      <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
+    </div>
+    
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const randomHexColor = () =>
+        `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component {
+    constructor () {
+        super ()
+    }
+
+    state = {
+        style: {
+            height: '60px', 
+            width: '50px',
+            backgroundColor: randomHexColor()
+        }
+    }
+
+    render () {
+        return (
+            <div style={this.state.style}></div>
+        )
+    }
+}
+export default Pixel
